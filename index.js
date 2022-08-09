73dab8acac6d38b473f4c7123928696d57156629
Sergii <s.kirianov@gmail.com>
2022-08-09 23:02:40 +0100

    remove env var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || '0.0.0.0';
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
