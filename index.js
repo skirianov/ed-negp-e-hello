e98b8195bf7f9eb70f150e805006c5628c98cbcb
Sergii <s.kirianov@gmail.com>
2022-08-09 22:47:29 +0100

    add env variables

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
